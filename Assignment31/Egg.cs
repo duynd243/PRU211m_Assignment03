d7d50c4c9b9136252a8f3bd80630e26849534c28
duyndse150396 <duyndse@gmail.com>
2022-06-26 14:00:52 +0700

    init

---
+++ Assignment31/Egg.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment1
+{
+    /// <summary>
+    /// An egg
+    /// </summary>
+    public class Egg
+    {
+        #region Fields
+
+        int amountLeft;
+        EggColor color;
+        HowCooked howCooked = HowCooked.NotCooked;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="size">egg size</param>
+        /// <param name="color">egg color</param>
+        public Egg(int size, EggColor color)
+        {
+            amountLeft = size;
+            this.color = color;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets how much egg is left to eat
+        /// </summary>
+        public int AmountLeft
+        {
+            get
+            {
+                // replace the line of code below with correct code
+                return this.amountLeft;
+            }
+        }
+
+        /// <summary>
+        /// Gets the color of the egg
+        /// </summary>
+        public EggColor Color
+        {
+            get
+            {
+                // replace the line of code below with correct code
+                return this.color;
+            }
+        }
+
+        /// <summary>
+        /// Gets how the egg is cooked
+        /// </summary>
+        public HowCooked CookedStatus
+        {
+            get
+            {
+                // replace the line of code below with correct code
+                return this.howCooked;
+            }
+        }
+
+        /// <summary>
+        /// Gets whether or not the egg is cooked
+        /// </summary>
+        public bool IsCooked
+        {
+            get
+            {
+                // replace the line of code below with correct code
+                return this.howCooked != HowCooked.NotCooked;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Cooks the egg
+        /// </summary>
+        /// <param name="howToCook">how the egg should be cooked</param>
+        public void Cook(HowCooked howToCook)
+        {
+            if(!IsCooked)
+            {
+                this.howCooked = howToCook;
+            }
+        }
+
+        /// <summary>
+        /// Takes a bite of the given size from the egg. Only takes
+        /// a bite if the egg is cooked
+        /// </summary>
+        /// <param name="size">size of the bite to take</param>
+        public void TakeBite(int size)
+        {
+            if (!IsCooked) return;
+            var newSize = AmountLeft- size;
+            this.amountLeft = (newSize >= 0) ? newSize : 0;
+        }
+
+        /// <summary>
+        /// Dyes the egg the given color. Only white eggs can be dyed,
+        /// and eggs can only be dyed blue
+        /// </summary>
+        /// <param name="color">color to dye the egg</param>
+        public void Dye(EggColor color)
+        {
+            
+            // My Comment: Only white eggs can be dyed, and they can only be dyed blue. 
+            if(this.color == EggColor.White && color == EggColor.Blue)
+            {
+                this.color = color;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assignment32/Matherator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment2
+{
+    /// <summary>
+    /// Provides a variety of numeric methods
+    /// </summary>
+    public class Matherator
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Matherator()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Prints the numbers from 1 to 10
+        /// </summary>
+        public void PrintOneToTen()
+        {
+            var result = "";
+            for (var i = 1; i <= 10; i++)
+            {
+                result += i + " ";
+            }
+            Console.WriteLine(result.Trim());
+        }
+
+        /// <summary>
+        /// Prints the numbers from m to n
+        /// </summary>
+        /// <param name="m">m</param>
+        /// <param name="n">n</param>
+        public void PrintMToN(int m, int n)
+        {
+            if (m > n)
+            {
+                Console.WriteLine("m must be less than or equal to n.");
+                return;
+            }
+            var result = "";
+            for (var i = m; i <= n; i++)
+            {
+                result += i + " ";
+            }
+            Console.WriteLine(result.Trim());
+        }
+
+        /// <summary>
+        /// Returns the tenth even number, with 2 as the first even number
+        /// </summary>
+        /// <returns>tenth even number</returns>
+        public int GetTenthEvenNumber()
+        {
+            // delete code below; only included so we could compile
+            return 2 + 9 * 2;
+        }
+
+        /// <summary>
+        /// Returns the nth even number, with 2 as the first even number
+        /// </summary>
+        /// <param name="n">n</param>
+        /// <returns>nth even number</returns>
+        public int GetNthEvenNumber(int n)
+        {
+            // delete code below; only included so we could compile
+            if (n < 1)
+                return -1;
+            return 2 + (n - 1) * 2;
+        }
+
+        #endregion
+    }
+}
